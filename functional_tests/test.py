7615123947a18d217740ded894144ad6fcda8055
nplatte <77999574+nplatte@users.noreply.github.com>
2025-04-01 17:43:31 -0400

    added first failing FT

---
+++ functional_tests/test.py
@@ -0,0 +1,21 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+class TestBasicPovertyMap(StaticLiveServerTestCase):
+
+    def setUp(self):
+        self.url = f"{self.live_server_url}/PA/schools/"
+        self.browser = Chrome()
+        return super().setUp()
+    
+    def tearDown(self):
+        return super().tearDown()
+    
+    def test_webpage_has_map(self):
+        # navigate to the page
+        response = self.browser.get(self.url)
+        # check title
+        self.assertEqual(self.browser.title, "PA School District Poverty Map")
+        # check that map exists
+        self.browser.find_element(By.ID, "State-Map")
--- poverty_tracker/settings.py
+++ poverty_tracker/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "functional_tests",
+    "school_districts"
 ]
 
 MIDDLEWARE = [
